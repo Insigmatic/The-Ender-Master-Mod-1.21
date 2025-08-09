ecfbf18254a305b2df844ad01a021865440a92fa
Jason <jasondylan2002@gmail.com>
2025-08-09 14:47:38 -0400

    Initial commit

---
+++ src/main/java/net/insigmatic/theendermaster/TheEnderMaster.java
@@ -0,0 +1,15 @@
+package net.insigmatic.theendermaster;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheEnderMaster implements ModInitializer {
+	public static final String MOD_ID = "theendermaster";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/insigmatic/theendermaster/TheEnderMasterClient.java
@@ -0,0 +1,10 @@
+package net.insigmatic.theendermaster;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheEnderMasterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
